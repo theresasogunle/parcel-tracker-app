67b3249b06d34ffc150df80a39d153d93b471d51
Tessa <sogunledolapo@gmail.com>
2018-07-25 10:53:16 -1200

    first commit

---
+++ app/src/main/java/com/deatt/parceltracker/MainActivity.java
@@ -0,0 +1,36 @@
+package com.deatt.parceltracker;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int menuItemThatWasSelected = item.getItemId();
+        if(menuItemThatWasSelected ==R.id.action_menu){
+            Context context =  MainActivity.this;
+
+            String message= "Menu clicked";
+
+            Toast.makeText(context,message,Toast.LENGTH_LONG).show();
+        }
+        return  true;
+    }
+}
